2821b34136a86e7f5dc34ae04aca82a32c7958d9
Cai JiaWei <1041028231@qq.com>
2025-04-06 10:35:20 +0800

    Initial Commit

---
+++ mindplay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mindplay
+//
+//  Created by 蔡佳伟 on 2025/4/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mindplay/mindplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindplayApp.swift
+//  mindplay
+//
+//  Created by 蔡佳伟 on 2025/4/6.
+//
+
+import SwiftUI
+
+@main
+struct mindplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
